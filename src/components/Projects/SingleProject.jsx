6a8b21dd09fead140ac3d0d5c7b09201a1f6061d
rakib8680 <rakib.cst.1st@gmail.com>
2023-07-02 18:49:44 +0600

    projects section done

--- src/components/Projects/SingleProject.jsx
+++ src/components/Projects/SingleProject.jsx
@@ -8,7 +8,7 @@ const singleProject = ({ project, index }) => {
     console.log(project);
     const { image, about, liveLink, clientCode, name, serverCode, technologies, flex } = project || {};
     return (
-        <div className={` md:flex ${flex} justify-center items-center gap-10 pb-20`}>
+        <div className={` md:flex ${flex} justify-center items-center gap-10 md:pb-20`}>
             <div className='hover:z-10 transition-all  duration-200 '
                 data-aos="fade-left"
                 data-aos-duration="800"
@@ -20,15 +20,28 @@ const singleProject = ({ project, index }) => {
                 data-aos="fade-right"
                 data-aos-duration="800"
                 data-aos-easing="ease-in-out">
-                <h1 className='font-black text-2xl text-center md:text-left'>{index + 1}. {name}</h1>
-                <p className='font-mono text-justify'>{about}</p>
+                <h1 className='font-black text-2xl text-center md:text-left'
+                    data-aos="fade-right"
+                    data-aos-duration="800"
+                    data-aos-easing="ease-in-out"
+                >{index + 1}. {name}</h1>
+                <p className='font-mono text-justify'
+                    data-aos="fade-left"
+                    data-aos-duration="800"
+                    data-aos-easing="ease-in-out">{about}</p>
                 <p className='font-bold'>Technologies :</p>
-                <p className='grid md:grid-cols-4 grid-cols-2 gap-2 pb-8'>
+                <p className='grid md:grid-cols-4 grid-cols-2 gap-2 pb-8'
+                    data-aos="fade-right"
+                    data-aos-duration="800"
+                    data-aos-easing="ease-in-out">
                     {
                         technologies.map(tech => <span key={tech} className='glowing-btn2 text-xs rounded-sm text-center font-mono'>{tech}</span>)
                     }
                 </p>
-                <div className='fancy-button  cursor-pointer flex flex-wrap justify-evenly gap-6 md:gap-0' >
+                <div className='fancy-button  cursor-pointer flex flex-wrap justify-evenly gap-6 md:gap-0'
+                    data-aos="fade-left"
+                    data-aos-duration="800"
+                    data-aos-easing="ease-in-out">
                     <span className='wow-btn '>
                         <a href={liveLink} target={'_blank'}> <FaGlobe className='!w-[20px] !h-[20px] mr-2' /> Live Site</a>
                     </span>
