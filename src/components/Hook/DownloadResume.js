b94248e872d269e0accc583cccd68d2fa6f7303c
rakib8680 <rakib.cst.1st@gmail.com>
2023-08-05 14:14:00 +0600

    installed react scroll

--- src/components/Hook/DownloadResume.js
+++
@@ -1,7 +0,0 @@
-const downloadResume = () => {
-    const resumeUrl = 'MyResume.pdf';
-    const anchor = document.createElement('a');
-    anchor.href= resumeUrl;
-    anchor.download= 'MyResume.pdf';
-    anchor.click();
-};
